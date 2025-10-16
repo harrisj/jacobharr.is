9cc7b00f989840b732ca2c5e49df283b0cb99d77
Jacob Harris <mail@jacobharr.is>
2025-10-15 20:08:35 -0400

    Trying some state storage

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,21 +1,21 @@
-import "bridgetown-quick-search"
-import "$styles/index.css"
-import "$styles/syntax-highlighting.css"
-import "bridgetown-lit-renderer"
+import "bridgetown-quick-search";
+import "$styles/index.css";
+import "$styles/syntax-highlighting.css";
+import "bridgetown-lit-renderer";
 
-import * as Turbo from "@hotwired/turbo"
+import * as Turbo from "@hotwired/turbo";
 
 // Uncomment the line below to add transition animations when Turbo navigates.
 // We recommend adding <meta name="turbo-cache-control" content="no-preview" />
 // to your HTML head if you turn on transitions. Use data-turbo-transition="false"
 // on your <main> element for pages where you don't want any transition animation.
 //
-import "./turbo_transitions.js"
+import "./turbo_transitions.js";
 
 // Import all JavaScript & CSS files from src/_components
-import components from "$components/**/*.{js,jsx,js.rb,css}"
+import components from "$components/**/*.{js,jsx,js.rb,css}";
 
 // import "@fontsource-variable/readex-pro"
 // import "@fontsource-variable/fira-code"
 
-console.info("Bridgetown is loaded!")
+console.info("Bridgetown is loaded!");
